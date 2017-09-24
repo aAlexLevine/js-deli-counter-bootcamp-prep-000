be1ca7d5dfc901b475c1ce161f06dfaacac33f94
Alex Levine <aalexlevine@gmail.com>
2017-09-24 18:04:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ return "There is nobody waiting to be served!"
 return `Currently serving ${array}.`
 }
 
-function currentLine(lineRightNow, katzDeliLine) {
+/*function currentLine(lineRightNow, katzDeliLine) {
 var lineRightNow = []
 if (katzDeliLine.length === 0)
   return "The line is currently empty."
@@ -21,3 +21,15 @@ for (let i =0; i < katzDeliLine.length; i++ ) {
 return lineRightNow
 }
 }
+*/
+
+var line = [];
+
+function currentLine(katzDeli) {
+  for (let i = 0; i < katzDeli.length; i++) {
+    line.push(` `+[i+1]+`. `  + katzDeli[i])
+  }
+  if (katzDeli.length === 0) {
+     return "The line is currently empty.";
+   } else
+  return(`The line is currently:` + line);
