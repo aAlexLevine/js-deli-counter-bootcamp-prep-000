454e217dc6a63ae147683f14aec5320387fc2792
Alex Levine <aalexlevine@gmail.com>
2017-09-24 15:59:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,8 +17,9 @@ var LineRightNow = []
 if (katzDeliLine.length === 0)
   return "The line is currently empty."
 for (let i =0; i < katzDeliLine.length; i++ ) {
-   LineRightNow.push(`The line is currently: ` + [i+1]`.` + katzDeliLine[i]);
+   LineRightNow.push(`The line is currently: ` + [i]`.` + katzDeliLine[i]);
 return LineRightNow
 }
-
 }
+
+
